228636fa295e3fb4707bd477dbb83bb054418370
teddy <teddyjh@protonmail.com>
2022-09-28 17:17:11 +0200

    Add file for SliderView

---
+++ Bullseye-v1/Bullseye-v1/Views/SliderView.swift
@@ -0,0 +1,20 @@
+//
+//  SliderView.swift
+//  Bullseye-v1
+//
+//  Created by Teddy Juhlin-Henricson on 2022-09-28.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderView()
+    }
+}
