a3024dca841c3b2e85428c47f76393af5ea36de3
teddy <teddyjh@protonmail.com>
2022-09-28 16:35:59 +0200

    Add file for Instruction Text

---
+++ Bullseye-v1/Bullseye-v1/Views/InstructionText.swift
@@ -0,0 +1,20 @@
+//
+//  InstructionText.swift
+//  Bullseye-v1
+//
+//  Created by Teddy Juhlin-Henricson on 2022-09-28.
+//
+
+import SwiftUI
+
+struct InstructionText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InstructionText_Previews: PreviewProvider {
+    static var previews: some View {
+        InstructionText()
+    }
+}
