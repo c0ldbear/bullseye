517232f366216d7b51e3adc1f1e592785dd4b8b1
teddy <teddyjh@protonmail.com>
2022-09-28 10:53:08 +0200

    Add project Bullseye-v1

---
+++ Bullseye-v1/Bullseye-v1/Bullseye_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Bullseye_v1App.swift
+//  Bullseye-v1
+//
+//  Created by Teddy Juhlin-Henricson on 2022-09-28.
+//
+
+import SwiftUI
+
+@main
+struct Bullseye_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye-v1/Bullseye-v1/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Bullseye-v1
+//
+//  Created by Teddy Juhlin-Henricson on 2022-09-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .kerning(-1.0)
+            HStack {
+                Text("1")
+                    .font(.headline)
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+                    .font(.headline)
+            }
+            Button(action: { print("button pressed!") }) {
+                Text("Hit me")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
