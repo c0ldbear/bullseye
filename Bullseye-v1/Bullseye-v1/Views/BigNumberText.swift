aad726ae9c5d24994c3fa0c1e05ec2ac74d85460
teddy <teddyjh@protonmail.com>
2022-09-28 16:43:29 +0200

    Add file for new struct: BigNumberText (target num)

---
+++ Bullseye-v1/Bullseye-v1/Views/BigNumberText.swift
@@ -0,0 +1,19 @@
+//
+//  BigNumberText.swift
+//  Bullseye-v1
+//
+//  Created by Teddy Juhlin-Henricson on 2022-09-28.
+//
+
+import SwiftUI
+
+struct BigNumberText: View {
+    var body: some View {
+    }
+}
+
+struct BigNumberText_Previews: PreviewProvider {
+    static var previews: some View {
+        BigNumberText()
+    }
+}
