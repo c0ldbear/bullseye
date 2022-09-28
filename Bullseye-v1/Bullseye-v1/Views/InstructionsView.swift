d55fd1b0e9c59049f3d9572f9fd119ba4237119d
teddy <teddyjh@protonmail.com>
2022-09-28 16:47:25 +0200

    Add file for all the views related to instructions

---
+++ Bullseye-v1/Bullseye-v1/Views/InstructionsView.swift
@@ -0,0 +1,20 @@
+//
+//  InstructionsView.swift
+//  Bullseye-v1
+//
+//  Created by Teddy Juhlin-Henricson on 2022-09-28.
+//
+
+import SwiftUI
+
+struct InstructionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InstructionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        InstructionsView()
+    }
+}
