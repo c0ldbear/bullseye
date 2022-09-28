e5c9b63088a6f2ecfe67d013a4fd7b3c16e2956b
teddy <teddyjh@protonmail.com>
2022-09-28 13:51:48 +0200

    Add model of the Game & show the randomised number

--- Bullseye-v1/Bullseye-v1/Views/ContentView.swift
+++ Bullseye-v1/Bullseye-v1/Views/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     
     @State private var alertIsVisible: Bool = false
     @State private var sliderValue: Double = 50.0
+    @State private var game: Game = Game()
     
     var body: some View {
         VStack {
@@ -20,7 +21,7 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
                 .lineSpacing(4.0)
                 .font(.footnote)
-            Text("89")
+            Text(String(game.target))
                 .font(.largeTitle)
                 .fontWeight(.black)
                 .kerning(-1.0)
@@ -39,7 +40,7 @@ struct ContentView: View {
             }
             .alert(isPresented: $alertIsVisible, content: {
                 let roundedValue: Int = Int(self.sliderValue.rounded())
-                return Alert(title: Text("Hello, there!"), message: Text("The slider value is \(roundedValue)") , dismissButton: .default(Text("Awesome!")))
+                return Alert(title: Text("Hello, there!"), message: Text("The slider value is \(roundedValue).\n" + "You scored  \(game.points(sliderValue: roundedValue)) points this round.") , dismissButton: .default(Text("Awesome!")))
             })
         }
     }
