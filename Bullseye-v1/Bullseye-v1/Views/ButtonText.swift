819e5a04c1eafeb44c38326e986839dc63efb091
teddy <teddyjh@protonmail.com>
2022-09-30 14:46:49 +0200

    Add files for new texts for custom alert view

---
+++ Bullseye-v1/Bullseye-v1/Views/ButtonText.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonText.swift
+//  Bullseye-v1
+//
+//  Created by Teddy Juhlin-Henricson on 2022-09-30.
+//
+
+import SwiftUI
+
+struct ButtonText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonText_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonText()
+    }
+}
