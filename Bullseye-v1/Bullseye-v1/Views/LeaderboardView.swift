53c52d35bd855fc42643a4c81e11a640acf63ed7
teddy <teddyjh@protonmail.com>
2022-10-03 10:14:52 +0200

    Add view for the leaderboard

---
+++ Bullseye-v1/Bullseye-v1/Views/LeaderboardView.swift
@@ -0,0 +1,20 @@
+//
+//  LeaderboardView.swift
+//  Bullseye-v1
+//
+//  Created by Teddy Juhlin-Henricson on 2022-10-03.
+//
+
+import SwiftUI
+
+struct LeaderboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LeaderboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        LeaderboardView()
+    }
+}
