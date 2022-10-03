85c0dca982db3903c2cfafbe23a1ba2d20e363b3
teddy <teddyjh@protonmail.com>
2022-10-03 10:39:00 +0200

    Add background color

--- Bullseye-v1/Bullseye-v1/Views/LeaderboardView.swift
+++ Bullseye-v1/Bullseye-v1/Views/LeaderboardView.swift
@@ -9,44 +9,48 @@ import SwiftUI
 
 struct LeaderboardView: View {
     var body: some View {
-        VStack {
-            ZStack {
-                Text("Leaderboard".uppercased())
-                    .font(.title)
-                    .fontWeight(.black)
-                    .kerning(2.0)
-                    .foregroundColor(Color("TextColor"))
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                ZStack {
+                    Text("Leaderboard".uppercased())
+                        .font(.title)
+                        .fontWeight(.black)
+                        .kerning(2.0)
+                        .foregroundColor(Color("TextColor"))
+                    HStack {
+                        Spacer()
+                        RoundedImageView(systemName: "xmark")
+                            .padding(.trailing)
+                    }
+                }
+                .padding(.leading)
+                .padding(.trailing)
+                //            .frame(maxWidth: Constants.Leaderboard.leaderboardMaxRowWidth)
+                
                 HStack {
                     Spacer()
-                    RoundedImageView(systemName: "xmark")
-                        .padding(.leading)
-                    .padding(.trailing)
+                        .frame(width: Constants.General.roundedViewLength)
+                    Spacer()
+                    Text("Score".uppercased())
+                        .font(.caption)
+                        .kerning(1.5)
+                        .multilineTextAlignment(.center)
+                        .frame(width: Constants.Leaderboard.leaderboardScoreColWidth)
+                    Spacer()
+                    Text("Date".uppercased())
+                        .font(.caption)
+                        .kerning(1.5)
+                        .multilineTextAlignment(.center)
+                        .frame(width: Constants.Leaderboard.leaderboardDateColWidth)
                 }
+                .padding(.top)
+                .padding(.leading)
+                .padding(.trailing)
+                .frame(maxWidth: Constants.Leaderboard.leaderboardMaxRowWidth)
+                RowView(index: 1, score: 999, date: Date())
             }
-            .padding(.leading)
-            .padding(.trailing)
-//            .frame(maxWidth: Constants.Leaderboard.leaderboardMaxRowWidth)
-            
-            HStack {
-                Spacer()
-                Spacer()
-                Text("Score".uppercased())
-                    .font(.caption)
-                    .kerning(1.5)
-                    .multilineTextAlignment(.center)
-                    .frame(width: Constants.Leaderboard.leaderboardScoreColWidth)
-                Spacer()
-                Text("Date".uppercased())
-                    .font(.caption)
-                    .kerning(1.5)
-                    .multilineTextAlignment(.center)
-                    .frame(width: Constants.Leaderboard.leaderboardDateColWidth)
-            }
-            .padding(.top)
-            .padding(.leading)
-            .padding(.trailing)
-            .frame(maxWidth: Constants.Leaderboard.leaderboardMaxRowWidth)
-            RowView(index: 1, score: 999, date: Date())
         }
     }
 }
@@ -57,5 +61,7 @@ struct LeaderboardView: View {
 struct LeaderboardView_Previews: PreviewProvider {
     static var previews: some View {
         LeaderboardView()
+        LeaderboardView()
+            .preferredColorScheme(.dark)
     }
 }
