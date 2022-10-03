6d38f01a54226e0737e79b40f91bc506f01c2e81
teddy <teddyjh@protonmail.com>
2022-10-03 15:07:34 +0200

    Add the struct code for the Book model

--- ReadMe/ReadMe/Book.swift
+++ ReadMe/ReadMe/Book.swift
@@ -5,4 +5,12 @@
 //  Created by Teddy Juhlin-Henricson on 2022-10-03.
 //
 
-import Foundation
+struct Book {
+    let title: String
+    let author: String
+    
+    init(title: String = "Title", author: String = "Author") {
+        self.title = title
+        self.author = author
+    }
+}
