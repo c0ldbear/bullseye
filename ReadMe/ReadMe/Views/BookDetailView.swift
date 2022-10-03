e99ab500d8e40e3b93aa6f17dead85782c3294c4
teddy <teddyjh@protonmail.com>
2022-10-03 16:10:44 +0200

    Add file for a detail view of a book

---
+++ ReadMe/ReadMe/Views/BookDetailView.swift
@@ -0,0 +1,22 @@
+//
+//  BookDetailView.swift
+//  ReadMe
+//
+//  Created by Teddy Juhlin-Henricson on 2022-10-03.
+//
+
+import SwiftUI
+
+struct BookDetailView: View {
+    let book: Book
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookDetailView()
+    }
+}
