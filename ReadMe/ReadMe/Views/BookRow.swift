561f71753651bf6ef5762d8eb9e3a541a44cec83
teddy <teddyjh@protonmail.com>
2022-10-03 15:52:53 +0200

    Add new file for BookRow

---
+++ ReadMe/ReadMe/Views/BookRow.swift
@@ -0,0 +1,20 @@
+//
+//  BookRow.swift
+//  ReadMe
+//
+//  Created by Teddy Juhlin-Henricson on 2022-10-03.
+//
+
+import SwiftUI
+
+struct BookRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookRow_Previews: PreviewProvider {
+    static var previews: some View {
+        BookRow()
+    }
+}
